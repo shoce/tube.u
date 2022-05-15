47fc9c13e1617b10a0af206d9db60e72cdb93b30
shoce <>
2022-05-15 15:21:54 +0530

    022.135.452

---
+++ go.mod
@@ -0,0 +1,39 @@
+module src.iriy.de/tube.u
+
+go 1.17
+
+require github.com/kkdai/youtube/v2 v2.7.13
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/dlclark/regexp2 v1.4.1-0.20201116162257-a2a8dda75c91 // indirect
+	github.com/dop251/goja v0.0.0-20220501172647-e1eca0b61fa9 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/kkdai/youtube v1.2.4 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.10.1 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/vbauerster/mpb/v5 v5.4.0 // indirect
+	golang.org/x/net v0.0.0-20220513224357-95641704303c // indirect
+	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ tube.u.go
@@ -0,0 +1,365 @@
+/*
+history:
+20/0410 v1
+20/1106 migrate to github.com/kkdai/youtube/v2
+
+go get -u -a -v
+go get -u -v github.com/kkdai/youtube/v2
+
+GoFmt GoBuildNull GoBuild GoRelease GoRun
+*/
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"os"
+	"path"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"unicode"
+
+	yt "github.com/kkdai/youtube/v2"
+)
+
+const (
+	NL = "\n"
+)
+
+var (
+	DownloadVideo bool
+
+	FilePerm   os.FileMode = 0644
+	Ctx        context.Context
+	HttpClient = &http.Client{}
+	YtCl       yt.Client
+	replacer   *strings.Replacer
+
+	youtubeRe, youtuRe, youtubeListRe, youtuListRe *regexp.Regexp
+
+	YoutubeREString     = `youtube.com/watch\?v=([0-9A-Za-z_-]+)`
+	YoutuREString       = `youtu.be/([0-9A-Za-z_-]+)$`
+	YoutubeListReString = `youtube.com/.*[?&]list=([0-9A-Za-z_-]+)$`
+	YoutuListReString   = `youtu.be/.*[?&]list=([0-9A-Za-z_-]+)$`
+
+	ConfigPath = "$HOME/config/youtube.keys"
+	YtKey      string
+
+	YtMaxResults = 50
+	TitleMaxLen  = 70
+)
+
+type YtPlaylistItemSnippet struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	PublishedAt string `json:"publishedAt"`
+	Thumbnails  struct {
+		Medium struct {
+			Url string `json:"url"`
+		} `json:"medium"`
+		High struct {
+			Url string `json:"url"`
+		} `json:"high"`
+		Standard struct {
+			Url string `json:"url"`
+		} `json:"standard"`
+		MaxRes struct {
+			Url string `json:"url"`
+		} `json:"maxres"`
+	} `json:"thumbnails"`
+	Position   int64 `json:"position"`
+	ResourceId struct {
+		VideoId string `json:"videoId"`
+	} `json:"resourceId"`
+}
+
+type YtPlaylistItem struct {
+	Snippet YtPlaylistItemSnippet `json:"snippet"`
+}
+
+type YtPlaylistItems struct {
+	NextPageToken string `json:"nextPageToken"`
+	PageInfo      struct {
+		TotalResults   int64 `json:"totalResults"`
+		ResultsPerPage int64 `json:"resultsPerPage"`
+	} `json:"pageInfo"`
+	Items []YtPlaylistItem
+}
+
+func init() {
+	Ctx = context.TODO()
+	YtCl = yt.Client{HTTPClient: &http.Client{}}
+	youtubeRe = regexp.MustCompile(YoutubeREString)
+	youtuRe = regexp.MustCompile(YoutuREString)
+	youtubeListRe = regexp.MustCompile(YoutubeListReString)
+	youtuListRe = regexp.MustCompile(YoutuListReString)
+
+	if path.Base(os.Args[0]) == "tube.v" {
+		DownloadVideo = true
+	}
+
+	if os.Getenv("YtKey") != "" {
+		YtKey = os.Getenv("YtKey")
+	}
+
+	if YtKey == "" {
+		ConfigPath = os.ExpandEnv(ConfigPath)
+		configBb, err := ioutil.ReadFile(ConfigPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "reading %s: %v", ConfigPath, err)
+		}
+
+		for _, configLine := range strings.Split(string(configBb), "\n") {
+			configLine = strings.TrimSpace(configLine)
+			if configLine == "" || strings.HasPrefix(configLine, "#") {
+				continue
+			}
+
+			kv := strings.Split(configLine, "=")
+			if len(kv) != 2 || strings.TrimSpace(kv[0]) == "" {
+				fmt.Fprintf(os.Stderr, "invalid %s config line: %s\n", ConfigPath, configLine)
+				continue
+			}
+
+			k, v := strings.TrimSpace(kv[0]), strings.TrimSpace(kv[1])
+			v = strings.Trim(v, `'"`)
+			if k == "YtKey" {
+				YtKey = v
+			}
+		}
+	}
+
+	if YtKey == "" {
+		fmt.Fprintln(os.Stderr, "No YtKey provided")
+		os.Exit(1)
+	}
+}
+
+func getJson(url string, target interface{}) error {
+	r, err := HttpClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func safestring(s string) (t string) {
+	for _, r := range s {
+		if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
+			r = '.'
+		}
+		t = t + string(r)
+	}
+
+	if len([]rune(t)) > TitleMaxLen {
+		t = string([]rune(t)[:TitleMaxLen])
+	}
+
+	return t
+}
+
+type YtVideo struct {
+	Id         string
+	NamePrefix string
+}
+
+func main() {
+	var err error
+	var ytid string
+	var NamePrefix string
+	var videos []YtVideo
+
+	if len(os.Args) > 2 {
+		NamePrefix = os.Args[2]
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: tube.u youtube.id [output.file.name.prefix]\nyoutube.id can be a youtube video or playlist url\n")
+		os.Exit(1)
+	}
+	ytid = os.Args[1]
+
+	var mm []string
+	if mm = youtubeListRe.FindStringSubmatch(ytid); len(mm) > 1 {
+		videos, err = getList(mm[1], NamePrefix)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+	} else if mm = youtuListRe.FindStringSubmatch(ytid); len(mm) > 1 {
+		videos, err = getList(mm[1], NamePrefix)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+	} else if mm = youtubeRe.FindStringSubmatch(ytid); len(mm) > 1 {
+		videos = []YtVideo{YtVideo{Id: mm[1], NamePrefix: NamePrefix}}
+	} else if mm = youtuRe.FindStringSubmatch(ytid); len(mm) > 1 {
+		videos = []YtVideo{YtVideo{Id: mm[1], NamePrefix: NamePrefix}}
+	}
+
+	var hadErrors bool
+
+	for _, i := range videos {
+		err = ytDownToFile(i.Id, i.NamePrefix)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			hadErrors = true
+			continue
+		}
+	}
+
+	if hadErrors {
+		os.Exit(1)
+	}
+
+	return
+}
+
+func getList(ytlistid, nameprefix string) (ytitems []YtVideo, err error) {
+	var videos []YtPlaylistItemSnippet
+	nextPageToken := ""
+
+	for nextPageToken != "" || len(videos) == 0 {
+		var PlaylistItemsUrl = fmt.Sprintf("https://www.googleapis.com/youtube/v3/playlistItems?maxResults=%d&part=snippet&playlistId=%s&key=%s&pageToken=%s", YtMaxResults, ytlistid, YtKey, nextPageToken)
+
+		var playlistItems YtPlaylistItems
+		err = getJson(PlaylistItemsUrl, &playlistItems)
+		if err != nil {
+			return nil, err
+		}
+
+		if playlistItems.NextPageToken != nextPageToken {
+			nextPageToken = playlistItems.NextPageToken
+		} else {
+			nextPageToken = ""
+		}
+
+		for _, i := range playlistItems.Items {
+			videos = append(videos, i.Snippet)
+		}
+	}
+
+	sort.Slice(videos, func(i, j int) bool { return videos[i].PublishedAt < videos[j].PublishedAt })
+	counterlen := int(math.Log10(float64(len(videos)))) + 1
+	numfmt := "%0" + strconv.Itoa(counterlen) + "d."
+
+	for vidnum, vid := range videos {
+		ytitems = append(
+			ytitems,
+			YtVideo{Id: vid.ResourceId.VideoId, NamePrefix: nameprefix + fmt.Sprintf(numfmt, vidnum+1)},
+		)
+	}
+
+	return ytitems, nil
+}
+
+func ytDownToFile(ytid, fpath string) error {
+	var err error
+
+	vinfo, err := YtCl.GetVideoContext(Ctx, ytid)
+	if err != nil {
+		return fmt.Errorf("GetVideoContext: %v", err)
+	}
+
+	var title string
+	if len([]rune(vinfo.Title)) > TitleMaxLen {
+		title = string([]rune(vinfo.Title)[:TitleMaxLen])
+	} else {
+		title = vinfo.Title
+	}
+	title = safestring(title)
+
+	//fmt.Fprintln(os.Stderr, vinfo.Description)
+
+	var streamfext string
+	var streamfmt yt.Format
+	if DownloadVideo {
+		for _, f := range vinfo.Formats {
+			if !strings.HasPrefix(f.MimeType, "video/") {
+				continue
+			}
+			if !strings.HasPrefix(f.MimeType, "video/mp4") || f.QualityLabel == "" || f.AudioQuality == "" {
+				continue
+			}
+			if f.Bitrate > streamfmt.Bitrate {
+				//fmt.Fprintf(os.Stderr, "Trying stream itag=%v \n", f.ItagNo)
+				if _, streamsize, err := YtCl.GetStreamContext(Ctx, vinfo, &f); err == nil && streamsize > 0 {
+					streamfext = "m4v"
+					streamfmt = f
+				}
+			}
+		}
+	} else {
+		for _, f := range vinfo.Formats {
+			if !strings.HasPrefix(f.MimeType, "audio/") {
+				continue
+			}
+			fmt.Fprintf(os.Stderr, "mimetype: %s"+NL, f.MimeType)
+			var fext string
+			if strings.HasPrefix(f.MimeType, "audio/mp4") {
+				fext = "m4a"
+			} else if strings.HasPrefix(f.MimeType, "audio/mp3") {
+				fext = "mp3"
+			} else {
+				continue
+			}
+			if f.Bitrate > streamfmt.Bitrate {
+				if _, streamsize, err := YtCl.GetStreamContext(Ctx, vinfo, &streamfmt); err == nil && streamsize > 0 {
+					streamfext = fext
+					streamfmt = f
+				}
+			}
+		}
+	}
+
+	if streamfmt.ItagNo == 0 {
+		return fmt.Errorf("could not find a working stream")
+	}
+
+	fpath = fmt.Sprintf("%s%s.%s", fpath, title, streamfext)
+
+	fmt.Fprintln(os.Stderr, fpath)
+
+	if fstat, err := os.Stat(fpath); !os.IsNotExist(err) && fstat.Size() > 0 {
+		return nil
+	}
+
+	streamrc, streamsize, err := YtCl.GetStreamContext(Ctx, vinfo, &streamfmt)
+	//fmt.Fprintf(os.Stderr, "Chose stream itag=%v mimetype=%s size=%v err=%v\n", streamfmt.ItagNo, streamfmt.MimeType, streamsize, err)
+	if err != nil {
+		return err
+	}
+	if streamsize == 0 {
+		return fmt.Errorf("stream size is zero")
+	}
+	defer streamrc.Close()
+
+	var streambuf bytes.Buffer
+	_, err = io.Copy(&streambuf, streamrc)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(
+		fpath,
+		streambuf.Bytes(),
+		FilePerm,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
